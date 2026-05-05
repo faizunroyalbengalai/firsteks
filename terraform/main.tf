d4f799753028f3b03af90e07f6cf7b7c5f395e54
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-05 20:45:16 +0600

    fix: Error: Unclosed configuration block on main.tf line 197, in  (attempt 5)

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,10 +20,10 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
@@ -197,3 +197,4 @@ data "aws_availability_zones" "available" {}
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
+}
